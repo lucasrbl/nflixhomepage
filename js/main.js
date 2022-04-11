a76b88b5564c7ed1f788145d9e7b3e5eba3120bc
lucasrbl <lucasrblemos@gmail.com>
2022-04-11 12:23:08 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function play(){
+    window.open("https://www.youtube.com/watch?v=YjEXQDE2u_E&ab_channel=Helena%27sRoom")
+}
+
+function moreinfo(){
+    open("https://myanimelist.net/anime/1604/Katekyo_Hitman_Reborn?q=katek&cat=anime")
+}
+
